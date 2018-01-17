435db7b9979369f86acd155806322a79beeabaa6
Matthias Steidel <maze.steidel@googlemail.com>
2018-01-17 10:46:51 +0100

    added attributes to StatisticalNode class

--- analyzer/src/main/java/model/statistics/StatisticalNode.java
+++ analyzer/src/main/java/model/statistics/StatisticalNode.java
@@ -1,5 +1,9 @@
 package model.statistics;
 
+import java.util.HashMap;
+
+import model.vessel.ShipType;
+
 /**
  * A {@link StatisticalNode} conforms exactly one node in the road network
  * model. It contains all statistical data.
@@ -8,5 +12,57 @@ package model.statistics;
  *
  */
 public class StatisticalNode {
+
+	private double lat;
+
+	private double lon;
+
+	private HashMap<String, Double> cogDistribution;
+
+	private HashMap<String, Double> sogDistribution;
+
+	private HashMap<ShipType, Double> shipTypeDistribution;
+
+	private HashMap<String, Double> lengthDistribution;
+
+	public StatisticalNode(double lat, double lon) {
+		this.cogDistribution = new HashMap<>();
+		this.sogDistribution = new HashMap<>();
+		this.shipTypeDistribution = new HashMap<>();
+		this.lat = lat;
+		this.lon = lon;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+
+	public HashMap<String, Double> getCogDistribution() {
+		return cogDistribution;
+	}
+
+	public HashMap<String, Double> getSogDistribution() {
+		return sogDistribution;
+	}
+
+	public HashMap<ShipType, Double> getShipTypeDistribution() {
+		return shipTypeDistribution;
+	}
+
+	public HashMap<String, Double> getLengthDistribution() {
+		return lengthDistribution;
+	}
 
 }
--- analyzer/src/main/java/view/MainWindow.java
+++ analyzer/src/main/java/view/MainWindow.java
@@ -1,12 +1,8 @@
 package view;
 
-import javax.swing.JFrame;
-
 public class MainWindow {
 
 	public static void startUI() {
-
-		JFrame mainFrame = new JFrame("AIS Analyzer");
 
 	}
 }
